2a7d9d1b2bfd7cdede92031f9704ab86675b4493
Christopher Schmitz <cschmitz@labelinsight.com>
2022-03-29 13:41:02 -0500

    adds base web api setup

---
+++ src/main/java/com/labelinsight/joltbuilderapi/JoltBuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.labelinsight.joltbuilderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltBuilderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoltBuilderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labelinsight/joltbuilderapi/JoltBuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labelinsight.joltbuilderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoltBuilderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
